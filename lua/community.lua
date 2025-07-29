bb330db930ba116f39903913dc2d7989d5e9c996
David Lyons <david@meshify.com>
2025-07-29 09:04:28 -0500

    remove rust

--- lua/community.lua
+++ lua/community.lua
@@ -14,7 +14,7 @@ return {
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.rust" },
+  --  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.helm" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.json" },
